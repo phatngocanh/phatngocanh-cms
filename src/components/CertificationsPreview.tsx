6024f57551b4353bce55f43e72427a93add5e477
VuKhoa23 <vuanhkhoa007@gmail.com>
2025-09-16 01:07:53 +0700

    fix: lint

--- src/components/CertificationsPreview.tsx
+++ src/components/CertificationsPreview.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const CertificationsPreview = () => {
     const [selectedCertificate, setSelectedCertificate] = useState<any>(null);
